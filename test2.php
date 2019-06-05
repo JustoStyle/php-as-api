208e80c35e5279ef3f73b1e473b8ac52c1631aca
Sam Burney <sburney@sifnt.net.au>
2019-06-05 11:10:18 +0000

    Allow test2.php node_id to be specified from the CLI

--- test2.php
+++ test2.php
@@ -3,6 +3,8 @@ require_once('vendor/autoload.php');
 require_once('include/config.php');
 require_once('ASAPI.php');
 
+$node_id = isset($argv[1]) ? $argv[1] : false;
+
 $asapi = new ASAPI([
     'username' => $username,
     'password' => $password,
@@ -10,6 +12,10 @@ $asapi = new ASAPI([
 
 // Enable or disable caching
 $nocache = false;
+
+if ($node_id) {
+    $data = $asapi->call('nodes/' + $node_id, $nocache);
+}
 
 // Ridgehaven - large working Node
 //$data = $asapi->call('nodes/195', $nocache);
